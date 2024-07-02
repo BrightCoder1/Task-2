f3e97366b166e9bffa1344aad0e9509bb1e2b4f1
Vishal Kumar <vs035967@gmail.com>
2024-07-02 13:04:07 +0530

    serverfile

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const path = require("path")
 const staticpath = path.join(__dirname, "./public/");
 
 app.set("view engine", "ejs");
+
 app.use(express.json());
 app.use(express.static(staticpath));
 app.use(express.urlencoded({
